ba8ae0e7032870b4c75517c907228f9a069cb082
MylesXR <119466865+MylesXR@users.noreply.github.com>
2024-05-04 01:29:05 -0600

    grappling hooks activate from explorer statue trigger, ref in explorer manager

--- Assets/PXR 2024/Scripts/Explorer/ExplorerManager.cs
+++ Assets/PXR 2024/Scripts/Explorer/ExplorerManager.cs
@@ -7,16 +7,22 @@ using VRC.Udon;
 public class ExplorerManager : UdonSharpBehaviour
 {
     public GlyphObjectManager GlyphObjectManager;
+    public GameObject grapplePC;
+    public GameObject grappleVR;
     void Start()
     {
-        
+
     }
     private void OnEnable()
     {
         GlyphObjectManager.SetAsExplorer();
+        grapplePC.SetActive(true);
+        grappleVR.SetActive(true);
     }
     private void OnDisable()
     {
         GlyphObjectManager.SetAsNotExplorer();
+        grapplePC.SetActive(false);
+        grappleVR.SetActive(false);
     }
 }
