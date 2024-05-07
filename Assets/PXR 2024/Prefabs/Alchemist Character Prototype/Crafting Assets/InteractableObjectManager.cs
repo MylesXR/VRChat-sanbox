18d6893649cda62e87048d74fb6b7b41e8a78168
ColePaskuski <cole.paskuski@outlook.com>
2024-05-07 00:31:36 -0600

    the amount of items you pick up is now visually represented in the Items tab of the Attendee Menu

--- Assets/PXR 2024/Prefabs/Alchemist Character Prototype/Crafting Assets/InteractableObjectManager.cs
+++ Assets/PXR 2024/Prefabs/Alchemist Character Prototype/Crafting Assets/InteractableObjectManager.cs
@@ -1,6 +1,6 @@
-﻿
-using UdonSharp;
+﻿using UdonSharp;
 using UnityEngine;
+using UnityEngine.UI;
 using VRC.SDKBase;
 using VRC.Udon;
 
@@ -10,28 +10,40 @@ public class InteractableObjectManager : UdonSharpBehaviour
     public int FlowersCollected = 0;
     public int GemstonesCollected = 0;
     public int PotionWallBreakerCollected = 0;
+
     public GameObject PotionWallBreaker;
+
+    // Add references to UI Text components
+    public Text HerbsText;
+    public Text FlowersText;
+    public Text GemstonesText;
+    public Text PotionWallBreakerText;
 
     public void IncrementHerbsCollected()
     {
         HerbsCollected++;
+        UpdateUI();
         Debug.Log($"HerbAmount: {HerbsCollected}");
     }
 
     public void IncrementFlowersCollected()
     {
         FlowersCollected++;
+        UpdateUI();
         Debug.Log($"Flowers collected: {FlowersCollected}");
     }
 
     public void IncrementGemstonesCollected()
     {
         GemstonesCollected++;
+        UpdateUI();
         Debug.Log($"Gemstones collected: {GemstonesCollected}");
     }
+
     public void IncrementPotionWallBreakerCollected()
     {
         PotionWallBreakerCollected++;
+        UpdateUI();
         Debug.Log($"Crafting items collected: {PotionWallBreakerCollected}");
     }
 
@@ -46,5 +58,21 @@ public class InteractableObjectManager : UdonSharpBehaviour
         {
             Debug.LogWarning("CraftingItem1 reference is missing!");
         }
+    }
+
+    // Method to update the UI text components
+    private void UpdateUI()
+    {
+        if (HerbsText != null)
+            HerbsText.text = $"{HerbsCollected}";
+
+        if (FlowersText != null)
+            FlowersText.text = $"{FlowersCollected}";
+
+        if (GemstonesText != null)
+            GemstonesText.text = $"{GemstonesCollected}";
+
+        if (PotionWallBreakerText != null)
+            PotionWallBreakerText.text = $"{PotionWallBreakerCollected}";
     }
 }
