98b628e2a260dc00cca27a3f74d2e0d5af02e4a8
MylesXR <119466865+MylesXR@users.noreply.github.com>
2024-07-01 20:30:25 -0600

    Players 1 and 2 can toggle the PCtether on and off. players can see eachothers tether.

--- Assets/PXR 2024/Scripts/Barbarian/AxeAssigner.cs
+++ Assets/PXR 2024/Scripts/Barbarian/AxeAssigner.cs
@@ -70,15 +70,50 @@ public class AxeAssigner : UdonSharpBehaviour
 
             if (axeToAssign.transform.childCount > -1)
             {
-                GameObject childGameObject = axeToAssign.transform.GetChild(0).gameObject;
-                childGameObject.SetActive(false);
-                Log($"Deactivated child game object: {childGameObject.name}");
-            }
-            if (axeToAssign.transform.childCount > -1)
-            {
-                GameObject childGameObject = axeToAssign.transform.GetChild(1).gameObject;
-                childGameObject.SetActive(true);
-                Log($"activated child game object: {childGameObject.name}");
+                //the PC axe in the head tracker prefab
+                GameObject pcAxeChild = axeToAssign.transform.GetChild(0).gameObject;
+                pcAxeChild.SetActive(false);
+                Log($"Deactivated child game object: {pcAxeChild.name}");
+
+
+
+                //The Return point for the VRaxe in the head tracker prefab
+                GameObject returnPointChild = axeToAssign.transform.GetChild(1).gameObject;
+                returnPointChild.SetActive(true);
+                Log($"activated child game object: {returnPointChild.name}");
+
+
+
+                //The parent of the pc tether objects in the head tracker prefab
+                GameObject pcTetherPartent = axeToAssign.transform.GetChild(3).gameObject;
+                pcTetherPartent.SetActive(false);
+                Log($"activated child game object: {pcTetherPartent.name}");
+                //Set ownership of all the pc tether objects children
+                foreach (Transform pcTetherLeftHandChild in pcTetherPartent.transform)
+                {
+                    Networking.SetOwner(player, pcTetherLeftHandChild.gameObject);
+                    Log($"Set ownership of child {pcTetherLeftHandChild.gameObject.name} to player {player.displayName}");
+                    pcTetherLeftHandChild.gameObject.SetActive(true);
+                }
+                //Set ownership of the pc tether objects left hand grandchildren
+                GameObject pcTetherLeftHandParent = pcTetherPartent.transform.GetChild(0).gameObject;
+                GameObject pcLeftTetherRingParent = pcTetherLeftHandParent.transform.GetChild(2).gameObject;
+                foreach (Transform pcLeftTetherRingGrandChild in pcLeftTetherRingParent.transform)
+                {
+                    Networking.SetOwner(player, pcLeftTetherRingGrandChild.gameObject);
+                    Log($"Set ownership of grandchild {pcLeftTetherRingGrandChild.gameObject.name} to player {player.displayName}");
+                    pcLeftTetherRingGrandChild.gameObject.SetActive(true);
+                }
+                //Set ownership of the pc tether object right hand grandchildren
+                GameObject pcTetherRightHandParent = pcTetherPartent.transform.GetChild(1).gameObject;
+                GameObject pcTetherRightRingParent = pcTetherRightHandParent.transform.GetChild(2).gameObject;
+                foreach (Transform pcTetherRightRingGrandChild in pcTetherRightRingParent.transform)
+                {
+                    Networking.SetOwner(player, pcTetherRightRingGrandChild.gameObject);
+                    Log($"Set ownership of grandchild {pcTetherRightRingGrandChild.gameObject.name} to player {player.displayName}");
+                    pcTetherRightRingGrandChild.gameObject.SetActive(true);
+                }
+
             }
         }
         else
