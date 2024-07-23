58ce4d136fc73eef739390ae9651a3d7b90cf3d0
MylesXR <119466865+MylesXR@users.noreply.github.com>
2024-07-23 00:06:54 -0600

    Audio zones code, many skyboxes, soup camp scene, tudor assets

---
+++ Assets/Zoltech/Scripts/Audio scripts/PlayerSpeakAudioZones.cs
@@ -0,0 +1,33 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace labthe3rd
+{
+    public class PlayerSpeakAudioZones : UdonSharpBehaviour
+    {
+        public GameObject toggleTest;
+        public VoiceBooster voiceBooster;
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+        {
+            if (player.isLocal)
+            {
+            voiceBooster.Trigger();
+            toggleTest.SetActive(true);
+            }
+        }
+
+        public override void OnPlayerTriggerExit(VRCPlayerApi player)
+        {
+            if (player.isLocal)
+            {
+            voiceBooster.Trigger();
+            toggleTest.SetActive(false);
+            }
+        }
+    }
+}
+
