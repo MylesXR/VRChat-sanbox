e9ef0ffb1447c9905402b34e8381ed2feadf1a7a
ColePaskuski <cole.paskuski@outlook.com>
2024-08-10 00:12:10 -0600

    Added outer buildings to the Advertising Hell Area

---
+++ Assets/PXR 2024/Scripts/Advertising Hell/VRC_Head_Attachment.cs
@@ -0,0 +1,28 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class VRC_Head_Attachment : UdonSharpBehaviour
+{
+    public GameObject modelToAttach; // The model you want to attach
+
+    void Start()
+    {
+        VRCPlayerApi playerApi = Networking.LocalPlayer; // Get the local player
+
+        if (playerApi != null && modelToAttach != null)
+        {
+            // Get the head tracking data position and rotation
+            Vector3 headPosition = playerApi.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position;
+            Quaternion headRotation = playerApi.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).rotation;
+
+            // Set the model's position and rotation to match the head's
+            modelToAttach.transform.position = headPosition;
+            modelToAttach.transform.rotation = headRotation;
+
+            // Optionally, you can add an offset if needed
+            // modelToAttach.transform.position += new Vector3(0, 0.2f, 0); // Example offset upwards
+        }
+    }
+}
