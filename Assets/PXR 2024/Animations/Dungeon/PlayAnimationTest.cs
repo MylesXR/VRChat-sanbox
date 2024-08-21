4772fe16489b796b417c9c18fb6499bc3ff311ae
ColePaskuski <cole.paskuski@outlook.com>
2024-08-21 04:41:09 -0600

    added updated dungeon models and props

---
+++ Assets/PXR 2024/Animations/Dungeon/PlayAnimationTest.cs
@@ -0,0 +1,17 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PlayAnimationTest : UdonSharpBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] string animationClip;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.Play(animationClip); // Replace with your animation clip name
+    }
+}
