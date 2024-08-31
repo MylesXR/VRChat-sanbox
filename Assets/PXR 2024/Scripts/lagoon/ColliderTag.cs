0ea238c492be3da04dfc8fd0231c98b014882386
MylesXR <119466865+MylesXR@users.noreply.github.com>
2024-08-31 03:21:07 -0600

    cave and crystals ani lagoon

---
+++ Assets/PXR 2024/Scripts/lagoon/ColliderTag.cs
@@ -0,0 +1,9 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ColliderTag : UdonSharpBehaviour
+{
+    public int colliderTag; // Public integer to identify this specific collider
+}
---
+++ Assets/PXR 2024/Scripts/lagoon/DisableObjectOnCollision.cs
@@ -0,0 +1,29 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class DisableObjectOnCollision : UdonSharpBehaviour
+{
+    public GameObject targetObject; // Reference to the GameObject that should be disabled
+    public string LockObject; // Name of the object that triggers the disable action
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object that entered the collider is the key object
+        if (other.gameObject.name == LockObject)
+        {
+            // Trigger the DisableObject method for all players
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "DisableObject");
+        }
+    }
+
+    public void DisableObject()
+    {
+        // Disable the GameObject
+        if (targetObject != null)
+        {
+            targetObject.SetActive(false);
+        }
+    }
+}
