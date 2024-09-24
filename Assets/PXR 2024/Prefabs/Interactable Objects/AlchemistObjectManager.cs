cb92f9cfc134fb26f67ea10be7f6b40a471d0828
ColePaskuski <90920336+ColePaskuski@users.noreply.github.com>
2024-09-24 16:02:58 -0600

    started to add the classes together with the statues

---
+++ Assets/PXR 2024/Prefabs/Interactable Objects/AlchemistObjectManager.cs
@@ -0,0 +1,41 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class AlchemistObjectManager : UdonSharpBehaviour
+{
+    public GameObject[] alchemistObjects;
+    private bool isAlchemist;
+
+    void Start()
+    {
+        isAlchemist = false;
+        ToggleBarbarianObjects();
+    }
+
+    public void SetAsBarbarian()
+    {
+        isAlchemist = true;
+        ToggleBarbarianObjects();
+    }
+
+    public void SetAsNotBarbarian()
+    {
+        isAlchemist = false;
+        ToggleBarbarianObjects();
+    }
+
+    public void ToggleBarbarianObjects()
+    {
+        foreach (GameObject obj in alchemistObjects)
+        {
+            Collider collider = obj.GetComponent<Collider>();
+            if (collider != null)
+            {
+                collider.enabled = isAlchemist;
+            }
+        }
+    } 
+}
