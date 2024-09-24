80677ef3a9a73583159c46bbd5f31843e9359811
MylesXR <119466865+MylesXR@users.noreply.github.com>
2024-09-24 14:30:56 -0600

    elevator and top town tavern update.

---
+++ Assets/Zoltech/Scripts/PlayAnimationSyncedCellDoors.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PlayAnimationSyncedCellDoors : UdonSharpBehaviour
+{
+    public Animator animator;
+    public string keyObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object that entered the collider is the key object
+        if (other.gameObject.name == keyObject)
+        {
+            // Trigger the animation for all players
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "AnimationPlay");
+        }
+    }
+    public void AnimationPlay()
+    {
+        animator.SetTrigger("PlayAnimation");
+    }
+
+}
