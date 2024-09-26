d15625ce237973a0e5ab90b2df348246e18f9361
ColePaskuski <90920336+ColePaskuski@users.noreply.github.com>
2024-09-26 17:13:01 -0600

    updated dungeon scene with puzzle II

---
+++ Assets/PXR 2024/Scripts/Dungeon/Puzzles/Puzzle_II_Explorer_Manager.cs
@@ -0,0 +1,17 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Puzzle_II_Explorer_Manager : UdonSharpBehaviour
+{
+    public Puzzle_II_Explorer_TriggerZone[] pressurePlates; // Array of all pressure plates
+
+    public void CheckPuzzleStatus()
+    {
+      
+        Debug.Log("Puzzle is complete! Triggering the next sequence.");
+        
+    }
+
+}
---
+++ Assets/PXR 2024/Scripts/Dungeon/Puzzles/Puzzle_II_Explorer_TriggerZone.cs
@@ -0,0 +1,43 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Puzzle_II_Explorer_TriggerZone : UdonSharpBehaviour
+{
+    public string correctObjectName; // The name of the correct object that needs to be placed on this pressure plate
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Make sure the other collider is not null
+        if (other == null || other.gameObject == null)
+        {
+            Debug.LogError("The collider or game object is null.");
+            return;
+        }
+
+        // Check if the object entering the trigger has the correct name
+        if (other.gameObject.name == correctObjectName)
+        {
+            Debug.Log("Correct object placed on pressure plate: " + other.gameObject.name);
+            // Call a method or trigger an event here
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Make sure the other collider is not null
+        if (other == null || other.gameObject == null)
+        {
+            Debug.LogError("The collider or game object is null.");
+            return;
+        }
+
+        // Handle logic if the object is removed from the pressure plate
+        if (other.gameObject.name == correctObjectName)
+        {
+            Debug.Log("Object removed from pressure plate: " + other.gameObject.name);
+            // Call a method or trigger an event here
+        }
+    }
+}
