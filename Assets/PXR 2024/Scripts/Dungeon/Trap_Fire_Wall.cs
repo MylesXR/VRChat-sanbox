62b8bc6f59c7b63e0745c9213a6b9528f5469eb8
ColePaskuski <90920336+ColePaskuski@users.noreply.github.com>
2024-09-27 01:41:53 -0600

    added fire wall trap to the dungeon and add the new hanging cage prefabs

---
+++ Assets/PXR 2024/Scripts/Dungeon/Trap_Fire_Wall.cs
@@ -0,0 +1,35 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Trap_Fire_Wall : UdonSharpBehaviour
+{
+    [SerializeField] private GameObject[] objectsToToggle;  // The game objects to toggle on/off
+    [SerializeField] private float toggleDelay = 1.0f;  // Public variable to set the delay in seconds
+    private bool areObjectsActive = true;
+
+    private void Start()
+    {
+        // Start the toggling process
+        ToggleObjects();
+    }
+
+    public void ToggleObjects()
+    {
+        // Toggle the active state of the objects
+        areObjectsActive = !areObjectsActive;
+
+        foreach (GameObject obj in objectsToToggle)
+        {
+            if (obj != null)
+            {
+                obj.SetActive(areObjectsActive);
+            }
+        }
+
+        // Schedule the next toggle using the specified delay
+        SendCustomEventDelayedSeconds(nameof(ToggleObjects), toggleDelay);
+    }
+}
