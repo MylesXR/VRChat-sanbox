98344c4cdfc7cb63cab08dc0b230f0bcd22ea7f6
ColePaskuski <90920336+ColePaskuski@users.noreply.github.com>
2024-09-30 16:00:24 -0600

    Added bridge animation and invisible collider for the barbarian section of Puzzle II in the Dungeon

---
+++ Assets/PXR 2024/Scripts/Dungeon/Puzzles/Puzzle_II_Bridge_Animations.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Puzzle_II_Bridge_Animations : UdonSharpBehaviour
+{
+    public Animator animator;
+
+    public void PlayAnimation()
+    {
+        if (animator != null)
+        {
+            animator.SetTrigger("PlayAnimation"); // Ensure your Animator has a trigger named "PlayAnimation"
+            Debug.Log("PlayAnimation trigger activated.");
+        }
+        else
+        {
+            Debug.LogError("Animator is not assigned.");
+        }
+    }
+}
