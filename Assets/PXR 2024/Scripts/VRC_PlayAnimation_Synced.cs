7830802474d38cc0f3a4eb56d96d774bde965433
ColePaskuski <90920336+ColePaskuski@users.noreply.github.com>
2024-10-07 15:23:36 -0600

    added a few scripts and animations for the dungeon

---
+++ Assets/PXR 2024/Scripts/VRC_PlayAnimation_Synced.cs
@@ -0,0 +1,21 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+
+public class VRC_PlayAnimation_Synced : UdonSharpBehaviour
+{
+    public Animator targetAnimation;
+
+    public override void Interact()
+    {
+        if (targetAnimation != null)
+        {
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "PlayAnimation");
+        }
+    }
+
+    public void PlayAnimation()
+    {
+        targetAnimation.SetTrigger("PlayAnimation");
+    }
+}
