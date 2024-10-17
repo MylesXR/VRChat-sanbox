d239f6c55282f3e45cbeea14a8584174a4c46d44
MylesXR <119466865+MylesXR@users.noreply.github.com>
2024-10-17 16:44:28 -0600

    launch party optomization

---
+++ Assets/PXR 2024/Scripts/OnTriggerToggleObjectOn.cs
@@ -0,0 +1,25 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class OnTriggerToggleObjectOn : UdonSharpBehaviour
+{
+    public GameObject objectToToggle;
+    void Start()
+    {
+
+    }
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (player.isLocal)
+        {
+            ToggleObject();
+        }
+    }
+    public void ToggleObject()
+    {
+        objectToToggle.SetActive(true);
+    }
+}
