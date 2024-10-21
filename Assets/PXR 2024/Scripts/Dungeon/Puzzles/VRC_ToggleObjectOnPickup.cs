473066ef6efc9e4e8cc14d4340aa24badfd4588f
ColePaskuski <90920336+ColePaskuski@users.noreply.github.com>
2024-10-20 20:31:46 -0600

    updated barbarian pickupables to use less batches when idle and set some meshes to static to static batch

--- Assets/PXR 2024/Scripts/Dungeon/Puzzles/VRC_ToggleObjectOnPickup.cs
+++ Assets/PXR 2024/Scripts/Dungeon/Puzzles/VRC_ToggleObjectOnPickup.cs
@@ -1,5 +1,6 @@
 ﻿using UdonSharp;
 using UnityEngine;
+using VRC.SDK3.Components;
 using VRC.SDKBase;
 using VRC.Udon;
 
@@ -8,9 +9,27 @@ public class VRC_ToggleObjectOnPickup : UdonSharpBehaviour
     public GameObject pickupObject;  // The object that will be toggled on and off
     public Collider pickupCollider;  // First collider to be disabled/enabled
     public Collider meshCollider;    // Second collider to be disabled/enabled
+    public VRC.SDK3.Components.VRCObjectSync objectSync; // Reference to VRC Object Sync
+
+    private void Start()
+    {
+        // Disable VRC Object Sync at the start
+        if (objectSync != null)
+        {
+            objectSync.enabled = false;
+            Debug.LogWarning("Object Sync OFF");
+        }
+    }
 
     public override void OnPickup()
     {
+        // Enable VRC Object Sync on pickup
+        if (objectSync != null)
+        {
+            objectSync.enabled = true;
+            Debug.LogWarning("Object Sync ON");
+        }
+
         if (pickupCollider != null)
         {
             // Disable the collider when picked up
@@ -25,6 +44,13 @@ public class VRC_ToggleObjectOnPickup : UdonSharpBehaviour
 
     public override void OnDrop()
     {
+        // Disable VRC Object Sync on drop
+        if (objectSync != null)
+        {
+            objectSync.enabled = false;
+            Debug.LogWarning("Object Sync OFF");
+        }
+
         if (pickupCollider != null)
         {
             // Re-enable the collider when dropped
