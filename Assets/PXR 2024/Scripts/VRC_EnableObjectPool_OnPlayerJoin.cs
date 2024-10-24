0556753887dbaa5336d879cb72f9c589f8f7c666
ColePaskuski <90920336+ColePaskuski@users.noreply.github.com>
2024-10-24 13:41:23 -0600

    dungeon optmization almost completed. changed some prefab locations for alchemist

---
+++ Assets/PXR 2024/Scripts/VRC_EnableObjectPool_OnPlayerJoin.cs
@@ -0,0 +1,13 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class VRC_EnableObjectPool_OnPlayerJoin : UdonSharpBehaviour
+{
+    void Start()
+    {
+        
+    }
+}
