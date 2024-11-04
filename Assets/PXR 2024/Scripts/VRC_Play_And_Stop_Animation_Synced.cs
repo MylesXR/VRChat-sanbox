c72183b4c81278da406378c4a9050a11d03df1e6
ColePaskuski <90920336+ColePaskuski@users.noreply.github.com>
2024-11-04 10:29:20 -0700

    updated the dungeon with  most of the changes needed for the attendees playthrough, added buttons to disable traps

---
+++ Assets/PXR 2024/Scripts/VRC_Play_And_Stop_Animation_Synced.cs
@@ -0,0 +1,55 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+public class VRC_Play_And_Stop_Animation_Synced : UdonSharpBehaviour
+{
+    public Animator[] Animators;
+    public string AnimationPlayTrigger = "PlayAnimation";
+    public string AnimationStopTrigger = "StopAnimation";
+    [UdonSynced] public bool isAnimationPlaying = false;
+
+    public override void Interact()
+    {
+        if (Animators != null && Animators.Length > 0)
+        {
+            if (!isAnimationPlaying)
+            {
+                SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "PlayAnimation");
+                isAnimationPlaying = true;
+            }
+            else
+            {
+                SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "StopAnimation");
+                isAnimationPlaying = false;
+            }
+        }
+    }
+
+    public void PlayAnimation()
+    {
+        if (Animators != null)
+        {
+            foreach (Animator animator in Animators)
+            {
+                if (animator != null)
+                {
+                    animator.SetTrigger(AnimationPlayTrigger);
+                }
+            }
+        }
+    }
+
+    public void StopAnimation()
+    {
+        if (Animators != null)
+        {
+            foreach (Animator animator in Animators)
+            {
+                if (animator != null)
+                {
+                    animator.SetTrigger(AnimationStopTrigger);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/PXR 2024/Scripts/VRC_ToggleObjectsOnOnce_Synced.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+
+public class VRC_ToggleObjectsOnOnce_Synced : UdonSharpBehaviour
+{
+    public GameObject[] targetObjects;
+
+    public override void Interact()
+    {
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "ToggleObject");
+    }
+
+    public void ToggleObject()
+    {
+        if (targetObjects != null)
+        {
+            foreach (GameObject targetObject in targetObjects)
+            {
+                targetObject.GetComponent<BoxCollider>().enabled = true;
+            }
+        }          
+    }
+}
