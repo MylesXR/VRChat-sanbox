d6a74eb253e635e9d54f5a17ed38cf3c80cc125d
ColePaskuski <90920336+ColePaskuski@users.noreply.github.com>
2024-11-09 10:01:48 -0700

    added last sounds for day one dungeon, built to Singlethread for day one, staff menu names updated

---
+++ Assets/PXR 2024/Scripts/Dungeon/PlayAudioAnimationEvent.cs
@@ -0,0 +1,17 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PlayAudioAnimationEvent : UdonSharpBehaviour
+{
+    public AudioSource audioSource; // Assign your AudioSource in the Inspector
+
+    public void PlaySound()
+    {
+        if (audioSource != null && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+}
