65540c2d457e5be8e78c67909d45d9adf94a3848
ColePaskuski <90920336+ColePaskuski@users.noreply.github.com>
2024-11-13 01:10:40 -0700

    changed compression on lots of textures for AD hell, added puzzle for ad hell

---
+++ Assets/PXR 2024/Scripts/Advertising Hell/Opening_Lock_With_Key.cs
@@ -0,0 +1,17 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+public class Opening_Lock_With_Key : UdonSharpBehaviour
+{
+    public GameObject keyObject;
+    public Animator lockAnimator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == keyObject)
+        {
+            lockAnimator.SetTrigger("PlayAnimation");
+            keyObject.SetActive(false);
+        }
+    }
+}
