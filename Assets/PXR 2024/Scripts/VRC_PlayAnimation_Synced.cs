be499b4035f6b215131ac8223b7c3202656962eb
ColePaskuski <90920336+ColePaskuski@users.noreply.github.com>
2024-11-15 01:17:29 -0700

    finished final logo animation just need it added into the new procsberg

--- Assets/PXR 2024/Scripts/VRC_PlayAnimation_Synced.cs
+++ Assets/PXR 2024/Scripts/VRC_PlayAnimation_Synced.cs
@@ -1,24 +1,83 @@
 ﻿using UdonSharp;
 using UnityEngine;
-using VRC.Udon.Common.Interfaces;
+using VRC.SDKBase;
+using VRC.Udon;
 
 public class VRC_PlayAnimation_Synced : UdonSharpBehaviour
 {
     public Animator Animator;
     public string AnimationTrigger = "PlayAnimation";
     [UdonSynced] private bool isAnimationPlaying;
+    [UdonSynced] private int animationPlayCount;
+
+    private int lastAnimationPlayCount;
+
+    void Start()
+    {
+        if (animationPlayCount > 0)
+        {
+            int timesToPlay = animationPlayCount - lastAnimationPlayCount;
+            for (int i = 0; i < timesToPlay; i++)
+            {
+                PlayAnimation();
+            }
+            lastAnimationPlayCount = animationPlayCount;
+        }
+    }
 
     public override void Interact()
     {
-        SendCustomNetworkEvent(NetworkEventTarget.All, "PlayAnimation");
+        if (!Networking.IsOwner(gameObject))
+        {
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        }
+
+        isAnimationPlaying = true;
+        animationPlayCount++;
+        RequestSerialization();
+
+        PlayAnimation();
+        ScheduleReset();
     }
 
-    public void PlayAnimation()
+    public override void OnDeserialization()
+    {
+        if (animationPlayCount != lastAnimationPlayCount)
+        {
+            int timesToPlay = animationPlayCount - lastAnimationPlayCount;
+            for (int i = 0; i < timesToPlay; i++)
+            {
+                PlayAnimation();
+                ScheduleReset();
+            }
+            lastAnimationPlayCount = animationPlayCount;
+        }
+    }
+
+    private void PlayAnimation()
     {
         if (Animator != null)
         {
-            isAnimationPlaying = !isAnimationPlaying; 
             Animator.SetTrigger(AnimationTrigger);
         }
+    }
+
+    private void ScheduleReset()
+    {
+        SendCustomEventDelayedSeconds(nameof(ResetAnimationState), Animator.GetCurrentAnimatorStateInfo(0).length);
+    }
+
+    public void ResetAnimationState()
+    {
+        if (!isAnimationPlaying)
+            return;
+
+        if (!Networking.IsOwner(gameObject))
+        {
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        }
+
+        isAnimationPlaying = false;
+        RequestSerialization();
     }
 }
