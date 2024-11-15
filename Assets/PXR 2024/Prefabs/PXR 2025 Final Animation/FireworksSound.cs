b885bd19cc57e53650fbd7390e2081109888ae8c
ColePaskuski <90920336+ColePaskuski@users.noreply.github.com>
2024-11-14 22:19:11 -0700

    changed stuff for the animation 2025

---
+++ Assets/PXR 2024/Prefabs/PXR 2025 Final Animation/FireworksSound.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class FireworksSound : UdonSharpBehaviour
+{
+    public AudioSource audioSource;  // Reference to the audio source to play
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+}
